2b303d7af534a58f53b0dcafef6ef7519ca3556f
Swapneel Mehta <swapneel.mehta96@gmail.com>
2018-10-25 17:13:06 +0200

    Renaming middleware for django

--- be/settings.py
+++ be/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'qa'
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
